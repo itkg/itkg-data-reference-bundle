8a5cdf8f9e014adc294b0e541afb26ca2238c47e
aurelien1 <aurelien.gilles@businessdecision.com>
2015-04-07 12:07:08 +0200

    suppression de commentaire et use statement inutilisé

--- ReferenceBundle/Facade/ReferenceFacade.php
+++ ReferenceBundle/Facade/ReferenceFacade.php
@@ -5,14 +5,12 @@ namespace Itkg\ReferenceBundle\Facade;
 use OpenOrchestra\ApiBundle\Facade\FacadeInterface;
 use OpenOrchestra\ApiBundle\Facade\DeletedFacade;
 use JMS\Serializer\Annotation as Serializer;
-use OpenOrchestra\ApiBundle\Facade\Traits\TimestampableFacade as Timestampable;
 
 /**
  * Class ReferenceFacade
  */
 class ReferenceFacade extends DeletedFacade
 {
-    //use Timestampable;
     /**
      * @Serializer\Type("string")
      */
