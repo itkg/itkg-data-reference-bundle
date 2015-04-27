dcf482d1769a4dd63302b33600dda2740b6d8c4e
aurelien1 <aurelien.gilles@businessdecision.com>
2015-04-27 16:06:26 +0200

    correction ligne en trop

--- ReferenceBundle/Transformer/ReferenceTypeTransformer.php
+++ ReferenceBundle/Transformer/ReferenceTypeTransformer.php
@@ -3,7 +3,6 @@
 namespace Itkg\ReferenceBundle\Transformer;
 
 use Itkg\ReferenceBundle\Document\ReferenceType;
-
 use Itkg\ReferenceBundle\Facade\ReferenceTypeFacade;
 use OpenOrchestra\Backoffice\Manager\TranslationChoiceManager;
 use OpenOrchestra\ApiBundle\Transformer\AbstractTransformer;
@@ -39,20 +38,7 @@ class ReferenceTypeTransformer extends AbstractTransformer
         foreach ($mixed->getFields() as $field) {
             $facade->addField($this->getTransformer('field_type')->transform($field));
         }
-/*
-        $facade->addLink('_self', $this->generateRoute(
-            'open_orchestra_api_content_type_show',//'open_orchestra_api_reference_type_show',
-            array('contentTypeId' => $mixed->getContentTypeId())//array('referenceTypeId' => $mixed->getReferenceTypeId())
-        ));
-        $facade->addLink('_self_delete', $this->generateRoute(
-            'open_orchestra_api_content_type_delete',
-            array('contentTypeId' => $mixed->getContentTypeId())//array('contentTypeId' => $mixed->getReferenceTypeId())
-        ));
-        $facade->addLink('_self_form', $this->generateRoute(
-            'open_orchestra_backoffice_content_type_form',//'open_orchestra_backoffice_reference_type_form',
-            array('contentTypeId' => $mixed->getContentTypeId())//array('referenceTypeId' => $mixed->getReferenceTypeId())
-        ));
-*/
+
         return $facade;
     }
 
