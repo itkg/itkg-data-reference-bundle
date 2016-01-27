518bcd683ec5767415c1b4d7042858a4e50252ae
guillaume le nouvel <guillaume.lenouvel@businessdecision.com>
2016-01-27 17:52:07 +0100

    restore default value for attributes

--- ReferenceBundle/EventSubscriber/ReferenceTypeSubscriber.php
+++ ReferenceBundle/EventSubscriber/ReferenceTypeSubscriber.php
@@ -8,6 +8,7 @@ use OpenOrchestra\ModelInterface\Model\FieldTypeInterface;
 use Itkg\ReferenceApiBundle\Repository\ReferenceTypeRepository;
 use Symfony\Component\Form\FormEvent;
 use Itkg\ReferenceInterface\Model\ReferenceTypeInterface;
+use Itkg\ReferenceInterface\Model\ReferenceInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -54,24 +55,30 @@ class ReferenceTypeSubscriber extends AbstractBlockReferenceTypeSubscriber
             foreach ($referenceType->getFields() as $referenceTypeField) {
 
                 if (isset($this->fieldTypesConfiguration[$referenceTypeField->getType()])) {
-                    $this->addFieldToForm($referenceTypeField, $form);
+                    $this->addFieldToForm($referenceTypeField, $form, $data);
                 }
             }
         }
     }
 
     /**
-     * Add $contentTypeField to $form with value $fieldValue
-     *
      * @param FieldTypeInterface $contentTypeField
      * @param FormInterface      $form
+     * @param ReferenceInterface $reference
      */
-    protected function addFieldToForm(FieldTypeInterface $contentTypeField, FormInterface $form)
+    protected function addFieldToForm(FieldTypeInterface $contentTypeField, FormInterface $form, ReferenceInterface $reference)
     {
+        $attribute = $reference->getAttributeByName($contentTypeField->getFieldId());
+        $defaultValue = $contentTypeField->getDefaultValue();
+        if ($attribute) {
+            $defaultValue = $attribute->getValue();
+        }
+
         $fieldTypeConfiguration = $this->fieldTypesConfiguration[$contentTypeField->getType()];
 
         $fieldParameters = array_merge(
             array(
+                'data'  => $defaultValue,
                 'label' => $this->translationChoiceManager->choose($contentTypeField->getLabels()),
                 'mapped' => false,
             ),
