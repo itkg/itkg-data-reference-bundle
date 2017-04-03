843f957421a300662817f3300390223e585c0d7c
itkg-ngilain <noel.gilain@businessdecision.com>
2017-04-03 16:29:05 +0200

    Filter reference menu if no required rights

--- ReferenceBundle/Resources/public/ecmascript/OpenOrchestra/Application/ReferenceSubApplication.js
+++ ReferenceBundle/Resources/public/ecmascript/OpenOrchestra/Application/ReferenceSubApplication.js
@@ -11,7 +11,20 @@ class ReferenceSubApplication
      * Run sub Application
      */
     run() {
+        this._initConfiguration();
         this._initRouter();
+    }
+
+    /**
+     * Initialize configuration
+     * @private
+     */
+    _initConfiguration() {
+        var user_roles = Application.getContext().user.roles;
+        Application.getContext().updateUserAccessSection(
+            'reference',
+            user_roles.indexOf('ROLE_DEVELOPER') > -1 || user_roles.indexOf('REFERENCE_ADMIN') > -1
+        );
     }
 
     /**
