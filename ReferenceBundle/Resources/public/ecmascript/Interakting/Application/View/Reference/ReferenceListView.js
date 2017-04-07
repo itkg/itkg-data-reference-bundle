6af8056c89ef8ae142f8a7b4ba6393962bf318be
itkg-ngilain <noel.gilain@businessdecision.com>
2017-04-07 15:43:11 +0200

    Fix reference list on reference type without attribute

--- ReferenceBundle/Resources/public/ecmascript/Interakting/Application/View/Reference/ReferenceListView.js
+++ ReferenceBundle/Resources/public/ecmascript/Interakting/Application/View/Reference/ReferenceListView.js
@@ -73,16 +73,18 @@ class ReferenceListView extends mix(AbstractDataTableView).with(UrlPaginateViewM
     _generateFieldColumn() {
         let columnsDefinition = [];
         let fields = this._referenceType.get('fields');
-        for (let field of fields) {
-            if (field.listable) {
-                columnsDefinition.push({
-                    name: "fields." + field.field_id + ".string_value",
-                    title: field.label,
-                    orderable: field.orderable,
-                    activateColvis: true,
-                    visible: true,
-                    createdCell: CellFormatterManager.format(field)
-                });
+        if (typeof fields !== "undefined") {
+            for (let field of fields) {
+                if (field.listable) {
+                    columnsDefinition.push({
+                        name: "fields." + field.field_id + ".string_value",
+                        title: field.label,
+                        orderable: field.orderable,
+                        activateColvis: true,
+                        visible: true,
+                        createdCell: CellFormatterManager.format(field)
+                    });
+                }
             }
         }
 
